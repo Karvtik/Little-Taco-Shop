39233415d2851d1d5dcd2bbdfa916fbfef3378d2
Karvtik <kartikvohra2003@gmail.com>
2024-07-24 16:15:56 +0530

    Finished with the JS file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// This is the JS file for displaying the Current Year in the footer section.
 const year=document.getElementById("year")
 const thisYear=new Date().getFullYear()
 year.setAttribute("datetime",thisYear)
